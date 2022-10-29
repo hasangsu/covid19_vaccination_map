d8d5f5c20aea8b58d54a4d4e9f9223fe46ce155c
hasangsu <ssha@autocrypt.io>
2022-10-29 13:50:09 +0900

    append map fragment naver map

--- app/src/main/java/com/example/covid19_vaccination_map/MapFragment.kt
+++ app/src/main/java/com/example/covid19_vaccination_map/MapFragment.kt
@@ -6,9 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.covid19_vaccination_map.databinding.MapFragmentBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
     private lateinit var binding: MapFragmentBinding
+    private lateinit var naverMap: NaverMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,5 +22,15 @@ class MapFragment : Fragment() {
     ): View? {
         binding = MapFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        var pos = CameraPosition(
+            LatLng(37.5300106, 126.9262812),
+            9.0)
+
+        naverMap.cameraPosition = pos
     }
 }
