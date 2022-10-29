cb597f4d6f08823a2459abe0b01bc4fd8e10ea76
hasangsu <ssha@autocrypt.io>
2022-10-29 12:56:00 +0900

    modify nav host default fragment

--- app/src/main/java/com/example/covid19_vaccination_map/MainActivity.kt
+++ app/src/main/java/com/example/covid19_vaccination_map/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.covid19_vaccination_map
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.covid19_vaccination_map.databinding.ActivityMainBinding
+import com.example.covid19_vaccination_map.databinding.MapFragmentBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
     }
 }
