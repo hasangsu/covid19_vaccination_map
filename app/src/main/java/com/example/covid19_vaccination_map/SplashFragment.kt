e9882c37d5c557bbc090c1f814e2d65257693bbe
hasangsu <ssha@autocrypt.io>
2022-10-29 13:15:08 +0900

    update splash fragment (change fragment)

--- app/src/main/java/com/example/covid19_vaccination_map/SplashFragment.kt
+++ app/src/main/java/com/example/covid19_vaccination_map/SplashFragment.kt
@@ -1,14 +1,20 @@
 package com.example.covid19_vaccination_map
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.covid19_vaccination_map.databinding.SplashFragmentBinding
+import kotlinx.coroutines.delay
 
 class SplashFragment : Fragment() {
     private lateinit var binding: SplashFragmentBinding
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,4 +25,21 @@ class SplashFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            navController.navigate(R.id.action_splashFragment_to_mapFragment)
+        }, 2000)
+
+    }
+
+    fun runAPI(no : Int) {
+    }
 }
